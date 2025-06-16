823b7d935c3af1d1f50d539ef9bb37cee03a9205
s2s2hyun <s2s2hyun0601@gmail.com>
2025-06-16 15:35:37 +0900

    [Fix] camera

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 export default function Home() {
@@ -14,6 +15,28 @@ export default function Home() {
     frameworkUrl: "unity/Build/Build.framework.js.br", // 이미 맞음
     codeUrl: "unity/Build/Build.wasm.br", // .br 추가 필요
   });
+
+  // 카메라 권한 요청하는 함수 추가
+  async function requestCameraPermission() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      console.log("카메라 권한 허용됨");
+      // 스트림 정리
+      stream.getTracks().forEach((track) => track.stop());
+      return true;
+    } catch (err) {
+      console.error("카메라 권한 거부됨:", err);
+      return false;
+    }
+  }
+
+  // Unity 로드 전에 권한 요청
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
 
   function handleClickSpawnEnemies() {
     if (isLoaded) {
